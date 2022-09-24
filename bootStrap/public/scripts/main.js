908bf8ce7a4d7c14a4eff67c59167a03bf2cdd8d
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-09-23 21:41:03 -0300

    testando funções de QuerySelector

--- bootStrap/public/scripts/main.js
+++ bootStrap/public/scripts/main.js
@@ -1,15 +1,23 @@
 /* It's printing "Hello world!" to the console. */
-console.log("Hello world!");
+// console.log("Hello world!");
 
 /* It's printing "Hello world!" to the alert box. */
-alert("hello world!");
+// alert("hello world!");
 
 /* It's asking the user for their name and storing it in the variable name. */
-const name = prompt("Qual é o su nome ? ");
+// const name = prompt("Qual é o su nome ? ");
 
 /**
  * This function prints a message to the console.
  */
 function imprimeNoConsole() {
   console.log("Função Imprime no console ativada com sucesso!");
+}
+
+function login() {
+  const email = document.querySelector("#email1").value;
+  const senha = document.querySelector("#senha1").value;
+
+  console.log("Email: ", email);
+  console.log("Senha: ", senha);
 }
