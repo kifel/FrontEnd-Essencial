6c148882a41273a50cd5b9b14e21d0730a06d615
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-09-23 21:19:46 -0300

    onclick function

--- bootStrap/public/scripts/main.js
+++ bootStrap/public/scripts/main.js
@@ -1,4 +1,15 @@
 /* It's printing "Hello world!" to the console. */
 console.log("Hello world!");
 
-alert('hello world!');
+/* It's printing "Hello world!" to the alert box. */
+alert("hello world!");
+
+/* It's asking the user for their name and storing it in the variable name. */
+const name = prompt("Qual é o su nome ? ");
+
+/**
+ * This function prints a message to the console.
+ */
+function imprimeNoConsole() {
+  console.log("Função Imprime no console ativada com sucesso!");
+}
